7997e1615d79ece76a4bd4bdae28316f448d350e
anna-makarchuk <amakarchuk1820@gmail.com>
2023-07-23 01:43:38 +0300

    hw7

---
+++ js/01-gallery.js
@@ -0,0 +1,50 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+console.log(gallery);
+const markup = markupImg(galleryItems);
+
+
+gallery.addEventListener('click',onImgContainerClick)
+
+function markupImg(galleryItems) {
+    return galleryItems.map(({ preview,original, description }) => {
+        return `
+        <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt=" ${description}"
+    />
+  </a>
+</li>
+        `;
+    }).join('');
+};
+gallery.insertAdjacentHTML('beforeend', markup);
+
+
+function onImgContainerClick(event) {
+    event.preventDefault();
+    for (let i = 0; i < galleryItems.length; i++) {
+       if (event.target.getAttribute('src') === galleryItems[i].preview) {
+            const instance = basicLightbox.create(
+            ` <img src="${galleryItems[i].original}" width="800" height="600"> `
+        );
+           instance.show();
+           document.body.addEventListener('keydown',(e)=> {
+    if (e.code === 'Escape') {
+        instance.close();
+    }
+
+  });
+
+       }
+    }
+};
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,31 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const markup = markupImg(galleryItems);
+
+
+function markupImg(galleryItems) {
+    return galleryItems.map(({ preview,original, description }) => {
+        return `
+       <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+        `;
+    }).join('');
+};
+gallery.insertAdjacentHTML('beforeend', markup);
+
+
+    let lightbox = new SimpleLightbox('.gallery a', {
+        captions: 'true',
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250
+    });
+    lightbox.open();
+
